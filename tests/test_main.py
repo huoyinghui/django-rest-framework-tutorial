9d0bf9b33fb95af5133f435d7439719d7805bc07
huoyinghui <37400712+huoyinghui@users.noreply.github.com>
2019-08-15 11:02:18 +0800

    add: test

---
+++ tests/test_main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Author: 
+Email:
+
+date: 
+desc:
+
+"""
+
+
+def test_main():
+    assert 1 == 1
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
